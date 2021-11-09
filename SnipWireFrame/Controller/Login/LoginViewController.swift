0d295ead1e1a3c94d7e4ee1ee1f8f55e73a9a9dd
zarinab92 <zarina.bekova@ext.myfitnesspal.com>
2021-11-09 13:10:09 -0500

    gradient change

--- SnipWireFrame/Controller/Login/LoginViewController.swift
+++ SnipWireFrame/Controller/Login/LoginViewController.swift
@@ -18,13 +18,13 @@ class LoginViewController: UIViewController {
         
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.type = .axial
-        gradient.colors = [UIColor.init(cgColor: .init(srgbRed: 255, green: 138, blue: 216, alpha: 100)), UIColor.white.cgColor]
+        gradient.colors = [UIColor.init(cgColor: .init(srgbRed: 300, green: 138, blue: 216, alpha: 100)), UIColor.white.cgColor]
         gradient.startPoint = CGPoint(x: 0, y: 0)
         gradient.endPoint = CGPoint(x: 1, y: 1)
         gradient.frame = view.bounds
 
         view.layer.insertSublayer(gradient, at: 0)
-
+ 
     }
     
     @IBAction func loginBtnTapped(_ sender: UIButton) {
@@ -58,7 +58,7 @@ class LoginViewController: UIViewController {
     }
     
     func showCreateAccount(email: String, password: String) {
-        let alert = UIAlertController(title: "Create Account", message: "Would you like to create an account", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Don't have one? Create New Account", message: "Would you like to create an account", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
             FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
                 guard let strongSelf = self else {
